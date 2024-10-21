73dbb512300ed9a539487dd89ecc94a0b31d4cd7
Graham Harrison <graham.harrison@axcendcorp.com>
2024-10-21 12:53:56 -0600

    matched version numbers

--- axcend_focus_operation/setup.py
+++ axcend_focus_operation/setup.py
@@ -4,7 +4,7 @@ package_name = 'axcend_focus_operation'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='3.1.5',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
